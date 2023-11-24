0bc9b96e37d4eba3d79e67af7b1b6541171a6e35
Clemence <clemence.l@somanity.com>
2023-11-24 11:09:34 +0100

    24-11 11:06 Uart marche raspi js et stm

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,6 +23,7 @@
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
